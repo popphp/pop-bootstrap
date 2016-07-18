a3836675468bedc848ec9bdabaf89cbb0c349686
Nick Sagona <nick@nolainteractive.com>
2016-07-18 09:47:12 -0500

    Add jax 5.0

--- public/assets/js/pop.js
+++ public/assets/js/pop.js
@@ -86,153 +86,17 @@ pop = {
             $('#session-timeout').remove();
             clearInterval(pop.sessionToInt);
 
-            var timeout = pop.cookie.load('pop_session_timeout');
-            var warning = pop.cookie.load('pop_timeout_warning');
+            var timeout = jax.cookie.load('pop_session_timeout');
+            var warning = jax.cookie.load('pop_timeout_warning');
             if ((timeout != '') && (warning != '')) {
                 pop.sessionToInt = setInterval(pop.timeoutWarning, (timeout - warning) * 1000);
             }
         }});
-    },
-
-    browser : function() {
-        var browser = {
-            "ua"      : navigator.userAgent,
-            "name"    : '',
-            "version" : '',
-            "device"  : '',
-            "os"      : ''
-        };
-
-        var os  = browser.ua.toLowerCase().match(/(windows|macintosh|linux|freebsd|openbsd|netbsd|sunos)/i);
-        var brw = browser.ua.toLowerCase().match(/(chrome|firefox|msie|trident|edge|konqueror|navigator|opera|safari)/i);
-        var dev = browser.ua.toLowerCase().match(/(android|blackberry|windows ce|windows phone|opera mini|pre|presto|ipod|iphone|ipad|nokia|symbian|palm|treo|hiptop|avantgo|plucker|xiino|blazer|elaine|teleca|up.browser|up.link|mmp|smartphone|midp|wap|vodafone|o2|pocket|kindle|mobile|pda|psp)/i);
-
-        if ((os != null) && (os[0] != undefined)) {
-            browser.os = os[0].charAt(0).toUpperCase() + os[0].substring(1).replace('bsd', 'BSD').replace('os', 'OS');
-        }
-
-        if ((brw != null) && (brw[0] != undefined)) {
-            if ((brw[0] == 'msie') || (brw[0] == 'trident') || (brw[0] == 'edge')) {
-                browser.name = 'MSIE';
-            } else {
-                browser.name = brw[0].charAt(0).toUpperCase() + brw[0].substring(1);
-            }
-        }
-
-        switch (browser.name) {
-            case 'Chrome':
-                browser.version = browser.ua.substring(browser.ua.indexOf('Chrome/') + 7);
-                browser.version = browser.version.substring(0, browser.version.indexOf(' '));
-                break;
-            case 'Firefox':
-                browser.version = browser.ua.substring(browser.ua.indexOf('Firefox/') + 8);
-                break;
-            case 'MSIE':
-                if (browser.ua.indexOf('Edge') != -1) {
-                    browser.version = browser.ua.substring(browser.ua.indexOf('Edge') + 4);
-                }
-                if (browser.ua.indexOf('MSIE ') != -1) {
-                    browser.version = browser.ua.substring(browser.ua.indexOf('MSIE ') + 5);
-                    browser.version = browser.version.substring(0, browser.version.indexOf(';'));
-                } else {
-                    browser.version = browser.ua.substring(browser.ua.indexOf('rv:') + 3);
-                    browser.version = browser.version.substring(0, browser.version.indexOf(')'));
-                }
-                break;
-            case 'Safari':
-                browser.version = browser.ua.substring(browser.ua.indexOf('Version/') + 8);
-                browser.version = browser.version.substring(0, browser.version.indexOf(' '));
-                break;
-        }
-
-        if ((dev != null) && (dev[0] != undefined)) {
-            browser.device = dev[0];
-        }
-
-        return browser;
-    },
-
-    cookie : {
-        save : function(name, value, options) {
-            if (typeof value != 'string') {
-                if (typeof value == 'number') {
-                    value = value.toString();
-                } else {
-                    value = JSON.stringify(value);
-                }
-            }
-            var cookie = name + '=' + encodeURI(value);
-
-            // Parse options
-            if (options != undefined) {
-                cookie += (options.domain != undefined) ? ';domain=' + options.domain : '';
-                cookie += (options.path != undefined) ? ';path=' + options.path : '';
-                if (options.expire != undefined) {
-                    var expdate = new Date();
-                    expdate.setDate(expdate.getDate() + options.expire);
-                    cookie += ';expires=' + expdate.toGMTString();
-                }
-            }
-
-            // Set the cookie.
-            document.cookie = cookie;
-
-            return this;
-        },
-        load : function(name) {
-            var value = '';
-
-            // If the cookie is set, parse the value.
-            if (document.cookie.length > 0) {
-                if (name == null) {
-                    value = {};
-                    var ary = document.cookie.split(';');
-                    for (var i = 0; i < ary.length; i++) {
-                        var a = ary[i].trim().split('=');
-                        var n = a[0];
-                        var v = decodeURI(a[1]);
-                        if ((v.indexOf('{') != -1) || (v.indexOf('[') != -1)) {
-                            v = JSON.parse(decodeURIComponent(v));
-                        }
-                        value[n] = v;
-                    }
-                } else {
-                    var start = document.cookie.indexOf(name + '=');
-
-                    if (start != -1) {
-                        start = start + name.length + 1;
-                        var end = document.cookie.indexOf(';', start);
-                        if (end == -1) {
-                            end = document.cookie.length;
-                        }
-
-                        value = decodeURI(document.cookie.substring(start, end));
-                        if ((value.indexOf('{') != -1) || (value.indexOf('[') != -1)) {
-                            value = JSON.parse(decodeURIComponent(value));
-                        }
-                    }
-                }
-            }
-
-            return value;
-        },
-        remove : function(name) {
-            if (name == null) {
-                var c = this.load();
-                for (var n in c) {
-                    this.save(n, '', {"expire" : -1});
-                }
-            } else {
-                this.save(name, '', {"expire" : -1});
-            }
-
-            return this
-        }
     }
 };
 
 $(document).ready(function(){
-    pop.cookie.save('pop_current_width', window.innerWidth, {path :'/'});
+    jax.cookie.save('pop_current_width', window.innerWidth, {path : '/'});
 
     if ($('#saved').data('saved') == 1) {
         $('#saved').fadeIn({complete : function(){
@@ -256,9 +120,8 @@ $(document).ready(function(){
     }
 
     if ($('#browser-detect')[0] != undefined) {
-        var browser = pop.browser();
-        if ((browser.name != '') && (browser.version != '')) {
-            $('#browser-detect')[0].innerHTML = browser.name + ' (' + browser.version + ')';
+        if ((jax.browser.name != '') && (jax.browser.version != '')) {
+            $('#browser-detect')[0].innerHTML = jax.browser.name + ' (' + jax.browser.version + ')';
         }
     }
 
@@ -347,8 +210,8 @@ $(document).ready(function(){
         });
     }
 
-    var timeout = pop.cookie.load('pop_session_timeout');
-    var warning = pop.cookie.load('pop_timeout_warning');
+    var timeout = jax.cookie.load('pop_session_timeout');
+    var warning = jax.cookie.load('pop_timeout_warning');
     if (timeout != '') {
         if (warning != '') {
             pop.sessionToInt = setInterval(pop.timeoutWarning, (timeout - warning) * 1000);
