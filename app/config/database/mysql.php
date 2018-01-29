f5b8b15a074c8e3a2ada04b5650b45cb284f7927
Nick Sagona <nick@nolainteractive.com>
2018-01-29 15:45:56 -0600

    Update license

---
+++ app/config/database/mysql.php
@@ -0,0 +1,10 @@
+<?php
+
+return [
+    'adapter'  => 'mysql',
+    'database' => 'popdb',
+    'username' => 'popuser',
+    'password' => '12pop34',
+    'host'     => 'localhost',
+    'type'     => ''
+];
